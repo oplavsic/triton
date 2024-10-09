f64d2009cd14199222488164a772cd0de7b896d8
Ognjen Plavsic <ognjen.plavsic@luxoft.com>
2024-10-10 01:52:15 +0200

    fix

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -169,7 +169,7 @@ class HIPBackend(BaseBackend):
         passes.ttgpuir.add_remove_layout_conversions(pm)
         amd.passes.ttgpuir.add_optimize_epilogue(pm)
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
-        amd.passes.ttgpuir.add_tritongpu_bypass_lds_for_dot_operand(pm)
+        # amd.passes.ttgpuir.add_tritongpu_bypass_lds_for_dot_operand(pm)
 
         use_new_pipeliner = os.getenv("TRITON_HIP_USE_NEW_STREAM_PIPELINE", "1") == "1"
         if amd.has_matrix_core_feature(options.arch):
